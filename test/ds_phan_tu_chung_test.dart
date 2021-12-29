4f42e3f36d1b8ccce5e08efc934986a338b68581
Luke Nguyen <luke.nguyen.goldenowl@gmail.com>
2021-12-29 17:52:48 +0700

    add test case

--- test/ds_phan_tu_chung_test.dart
+++ test/ds_phan_tu_chung_test.dart
@@ -7,4 +7,9 @@ void main() {
     List<int> list2 = [];
     expect(dsPhanTuChung(list1, list2), []);
   });
+  test('Phần từ chung của cả 2 mảng cho trước, không trùng nhau', () {
+    List<int> list1 = [1, 2, 3];
+    List<int> list2 = [3, 4, 5];
+    expect(dsPhanTuChung(list1, list2), [3]);
+  });
 }
