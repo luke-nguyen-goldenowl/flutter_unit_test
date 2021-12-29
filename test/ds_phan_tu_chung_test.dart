695c70f1b3cc49d329af42bb7216adee997e0dd4
Luke Nguyen <luke.nguyen.goldenowl@gmail.com>
2021-12-29 18:03:48 +0700

    fix text in test case 1

--- test/ds_phan_tu_chung_test.dart
+++ test/ds_phan_tu_chung_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_unit_test/constants/ds_phan_tu_chung.dart';
 
 void main() {
-  test('Phần từ chung của cả 2 mảng cho trước, không trùng nhau', () {
+  test('Phần từ chung của 2 mảng rỗng', () {
     List<int> list1 = [];
     List<int> list2 = [];
     expect(dsPhanTuChung(list1, list2), []);
